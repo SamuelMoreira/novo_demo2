9ba02c55d3add748f18b4257a9cb8426fc8f2958
Samuel Moreira <genesis.samuel@gmail.com>
2011-11-02 22:39:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NovoDemo2::Application.config.secret_token = 'ce052ccff63f8cc2d6d81c19a84c75041de7fc3f511328af879d3ee7ddc51f2ee0048df1126958eb6dc873411176f45549210245254a69eba32c5dd4d9da301b'
